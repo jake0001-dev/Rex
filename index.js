71e21736079931a5cf56b6480a58e7bb076b15dd
Jake <59755030+jake0001-dev@users.noreply.github.com>
2023-09-13 23:19:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const {WebSocketServer} = require('ws')
 var sql_client = require('sql-client')
 
 const {username, password,email, serverid } = require('./dummy_data.json')
-const wss = new WebSocketServer({port: 8080})
+const wss = new WebSocketServer({port: 6666})
 
 async function user_auth(user) {
     if (user.username == username && user.password == password) {
-        return true
+        return 200
     } else {
-        return false
+        return 401
     }
 }
 
